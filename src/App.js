56c0c634b4b858fefd5598c9969f5cd18e6a2e74
iamgaara <thenameisgaara07@gmail.com>
2024-07-31 13:29:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
             <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<ContactAndFAQ />} />
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<contact />} />
           </Routes>
         </main>
         <Footer />
